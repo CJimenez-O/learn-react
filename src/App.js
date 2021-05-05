25985f5a1bf234ef03e1f5fcba848f4a592929da
legazy33 <chris.jimenez50@yahoo.com>
2021-05-04 21:02:29 -0700

    Learning react basics

--- src/App.js
+++ src/App.js
@@ -1,22 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.scss";
+import NewName from "./components/newName";
+import Children from "./components/newChild";
+
+// REACT HOOKS RULES
+// -Dont call inside loops
+// -Dont call inside conditions
+// -Dont call inside nested function
+// -Use on top of function
+// -Only call hooks from react function
+
+const makeGreen = (BaseComponent) => (props) => {
+  const addGreen = {
+    style: {
+      color: "green",
+    },
+  };
+
+  // passes prop and green variable to newProp
+  const newProp = {
+    ...props,
+    ...addGreen,
+  };
+
+  // newProp gets added to BaseComp
+  return <BaseComponent {...newProp} />;
+};
+
+//New tag is created by passing in a Tag to the function
+const GreenNameTag = makeGreen(NewName);
+
+const initialNames = [
+  { firstName: "Chris", lastName: "Jimenez" },
+  { firstName: "Roger", lastName: "Perez" },
+];
 
 function App() {
+  // [variable, function]
+  const [age, setAge] = useState(21);
+  const [names, setNames] = useState(initialNames);
+  const ageUpHandle = () => {
+    setAge(age + 1);
+  };
+  const ageDownHandle = () => {
+    setAge(age - 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Use State Hook</h1>
+        <h2>Age: {age}</h2>
+        <button onClick={ageUpHandle}>Age up</button>
+        <button onClick={ageDownHandle}>Age down</button>
+        <br></br>
+
+        <NewName firstName="Hector" lastName="Cruz"></NewName>
+
+        {/* Create tags easier */}
+        {names.map((v, i) => {
+          return (
+            <NewName
+              /* Create key so react knows what is changing */
+              key={`${i}${v.firstName}${v.lastName}`}
+              firstName={v.firstName}
+              lastName={v.lastName}
+            ></NewName>
+          );
+        })}
+
+{/* 
+<div className="App">
+<header className="App-header">
+<h1>Names</h1>
+<NewName firstName="propName" lastName="fragment"></NewName>
+<GreenNameTag firstName="Chris" lastName="Jimenez"></GreenNameTag>
+<Children>Children</Children>
+<NewName> Test </NewName>
+</header>
+</div>
+*/}
       </header>
     </div>
   );
